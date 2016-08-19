c207e9c1c9f8d036875062bbc40153ead206930e
Shangyao Lin <1210tom@163.com>
2016-08-19 12:17:54 +0800

    python flake8 检查修改

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -4,8 +4,10 @@
 	  (lambda ()
 	    (elpy-mode 1)
 	    (setq elpy-rpc-backend "jedi")
-	    ; (setq indent-tabs-mode t)
+	    (setq indent-tabs-mode t)
+	    (setq python-check-command "flake8 --ignore=E501,E302,E231,W191")
 	    (setq python-indent 4)
+	    (setq tab-width 4)
 		(add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
 (add-hook 'python-mode-hook 'guess-style-guess-tabs-mode)
