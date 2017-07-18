c4109c34a32b4ed20987e543fc68297f92c4e474
林上耀 <1210tom@163.com>
2017-07-18 09:50:49 +0800

    显示行尾空白，fic-mode高亮TODO,FIXME

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,18 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (sanityinc-solarized-dark))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; 设置字体
 (set-default-font "monaco 12")
 ;;设置默认读入文件编码
@@ -24,18 +9,14 @@
 ;; tab set 4 space
 (setq tab-width 4)
 
-;;------------------------------------------------------------------
-;;%% 包配置
-;;------------------------------------------------------------------
-(projectile-global-mode)
-
-
-
-(require 'window-numbering)
-(window-numbering-mode 1)
+; (require 'window-numbering)
+; (window-numbering-mode 1)
 
 ;; 自动从硬盘加载
 (global-auto-revert-mode t)
 (setq global-auto-revert-non-file-buffers t)
-
+(require 'fic-mode)
+(add-hook 'prog-mode-hook 'fic-mode)
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 (provide 'init-misc)
