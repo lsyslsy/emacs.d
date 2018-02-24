9ae83baa694527efe42241dba9ff0f514ab5235d
林上耀 <1210tom@163.com>
2018-02-24 15:08:53 +0800

    修正custom.el里面的safe local variable的错误。把字体大小改成15

--- custom.el
+++ custom.el
@@ -24,12 +24,10 @@
     ((projectile-enable-caching . t)
      (ffip-find-options . "-not -size +64k -not -iwholename '~/LX/development/server/*'")
      (ffip-project-root . "~/lx/development/server")
-     (ffip-patterns "*.py" "*.cs")
-     (ffip-project-root . "~/LX/development/server")
-     (ffip-find-options . "-not -size +64k -not -iwholename '~/lx/development/server/*'")))))
+     (ffip-patterns . ("*.py" "*.cs"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Monaco")))))
