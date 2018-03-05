96f2819da752933ed80d9e0ed0d17865e2b7e7a4
林上耀 <1210tom@163.com>
2018-03-05 15:49:34 +0800

    使用eval-after-load，加快启动速度，python相关设置在需要时加载

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,4 +1,6 @@
 (add-hook 'c++-mode-hook 'flycheck-mode)
 (add-hook 'c-mode-hook 'flycheck-mode)
+;; grammal check: flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode);global enable
 
 (provide 'init-flycheck)
--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,23 +1,29 @@
 ;(require 'guess-style)
-(require 'jedi)
 ;(setq jedi:server_args
 ;	'("--virtual-env" "~/.virtualenvs/lx"))
 
 
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (elpy-mode 1)
-	    (setq elpy-rpc-backend "jedi")
-	    ;(setq python-check-command "/usr/local/bin/flake8 --ignore=E501,E302,E231,W191,E128,E261,E251,E203,E303,E226")
-	    (setq python-check-command "pylint --rcfile=~/lx/development/server/yao.lint")
+(with-eval-after-load 'python
+    (require 'jedi)
+    (add-hook 'python-mode-hook
+              (lambda ()
+                (elpy-mode 1)
+                (setq elpy-rpc-backend "jedi")
+                                        ;(setq python-check-command "/usr/local/bin/flake8 --ignore=E501,E302,E231,W191,E128,E261,E251,E203,E303,E226")
+                (setq python-check-command "pylint --rcfile=~/lx/development/server/yao.lint")
  		(setq elpy-syntax-check-command "pylint --rcfile=~/lx/development/server/yao.lint")
  		(setq flycheck-highlighting-mode 'lines)
 		(setq python-indent 4)
 		(setq flycheck-idle-change-delay 2)
-	    (setq tab-width 4)))
+                (setq tab-width 4)))
 
-	    ;; grammal check: flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode);global enable
+    
+    ; set key binding
+    (define-key python-mode-map (kbd "M-.") 'jedi:goto-definition)
+    (define-key python-mode-map (kbd "M-,") 'jedi:goto-definition-pop-marker)
+    (define-key python-mode-map (kbd "M-?") 'jedi:show-doc)
+    (define-key python-mode-map (kbd "M-/") 'jedi:get-in-function-call))
+
                     ; close flymake,  start flycheck
 ; (when (require 'flycheck nil t)
 ;   (setq elpy-modules(delq 'elpy-module-flymake elpy-modules))
@@ -35,13 +41,6 @@
 ;       (add-hook 'python-mode-hook
 ;                  'jedi-config:set-python-executable))
 
-;  jedi 使用autocomplete作为后端
-(defun jedi-config:setup-keys ()
-       (define-key python-mode-map (kbd "M-.") 'jedi:goto-definition)
-       (define-key python-mode-map (kbd "M-,") 'jedi:goto-definition-pop-marker)
-       (define-key python-mode-map (kbd "M-?") 'jedi:show-doc)
-       (define-key python-mode-map (kbd "M-/") 'jedi:get-in-function-call))
-(add-hook 'python-mode-hook 'jedi-config:setup-keys)
 
 
 (provide 'init-python-mode)
