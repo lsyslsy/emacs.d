913e60bc1f81d283dd0f194b0ca215c365cbc453
林上耀 <1210tom@163.com>
2018-03-15 13:58:12 +0800

    evil 集成 ， helm, helm-projctile集成，使用helm-M-x替代smex的

--- init.el
+++ init.el
@@ -18,12 +18,13 @@
 (require 'init-dired)
 (require 'init-python-mode)
 (require 'init-project)
-(require 'helm-config)
+(require 'init-helm)
 ;(require 'init-helm-gtags)
 ;(require 'init-irony)
 ;(require 'init-rtags)
 (require 'init-cmake-ide)
 (require 'init-flycheck)
+(require 'init-evil)
 
 ;;; 默认不用tab，因为tab宽度问题，可能会引来一系列问题
 ;; 默认setq-default设置默认值，buffer自己的local值会覆盖着
@@ -39,8 +40,17 @@
 ; 默认是调用设置fill-column的长度的，实在基类, 还容易和查找文件搞混
 (global-unset-key "\C-xf")
 
+; stop cursor blick, because bothering
+(blink-cursor-mode 0)
 
 ;; customize setting file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+
+;;  for helm swoop
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
 
---
+++ lisp/init-helm.el
@@ -0,0 +1,32 @@
+(require 'helm)
+(require 'helm-config)
+
+; 去掉默认的C-x c绑定，还是绑定到C-c h
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+; 替换默认的buffer
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+;(setq helm-autoresize-max-height 0)
+;(setq helm-autoresize-min-height 20)
+;(helm-autoresize-mode 1)
+(require 'helm-projectile)
+(helm-projectile-on)
+
+(provide 'init-helm)
