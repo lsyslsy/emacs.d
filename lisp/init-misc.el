80cfc91e8591ba231d29f687e78515cf8aaeee27
林上耀 <1210tom@163.com>
2018-03-15 13:56:49 +0800

    mac PATH 路径问题   ，有道词典

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -20,10 +20,12 @@
 (add-hook 'prog-mode-hook 'fic-mode)
 ;; show unncessary whitespace that can mess up your diff
 (add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+;; 下面会显著增加启动时间，大约1.4s
 (when (memq window-system '(mac ns x))
+  ; 设成nil 则不从 .zshrc 读 只从 .zshenv读（可以加快速度，但是需要你将环境变量相关的都放到 .zshenv 中，而非 .zshrc 中）
+  (setq exec-path-from-shell-check-startup-files nil) ;
+  (setq exec-path-from-shell-arguments '("-l" )) ;remove -i read form .zshenv
   (exec-path-from-shell-initialize))
-
-;; 关掉光标闪动，看起来确实有点不爽
-(if (fboundp 'blick-cursor-mode)
-    (blink-cursor-mode -1))
+;; youdao dictionary key binding
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
 (provide 'init-misc)
