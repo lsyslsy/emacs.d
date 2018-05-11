56acfefda38173a947fc37337ade0f1f43d3c2bf
林上耀 <1210tom@163.com>
2018-05-11 16:11:29 +0800

    去掉elpa-stable，默认都选择非稳定最新版

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -2,7 +2,7 @@
 (setq package-archives '(
 		("gnu" . "http://elpa.gnu.org/packages/")
 		("melpa" . "http://melpa.org/packages/")
-		("melpa-stable" . "http://stable.melpa.org/packages/")
+		;("melpa-stable" . "http://stable.melpa.org/packages/")
 ))
 (package-initialize)
 
