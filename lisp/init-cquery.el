039efb4c0d9d84b54e310395bdc250da032507ee
林上耀 <1210tom@163.com>
2018-11-22 20:53:46 +0800

    modify cquery support, stop nuse helm etags select

--- lisp/init-cquery.el
+++ lisp/init-cquery.el
@@ -5,6 +5,15 @@
 
 (defun cquery-enable ()
   (lsp-cquery-enable)
+  (define-key evil-normal-state-map (kbd "C-p") 'lsp-ui-peek-jump-forward)
+  (define-key evil-normal-state-map (kbd "C-t") 'lsp-ui-peek-jump-backward)
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  ;; Alternatively, use lsp-ui-peek interface
+  (lsp-ui-peek-find-custom 'base "$cquery/base")
+  (lsp-ui-peek-find-custom 'callers "$cquery/callers")
+  (lsp-ui-peek-find-custom 'random "$cquery/random") ;; jump to a random declaratio
+  (lsp-ui-peek-find-workspace-symbol "pattern 0")
 )
 
 (add-hook 'c-mode-hook #'cquery-enable)
--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -35,14 +35,14 @@
 (my-install-package 'helm-swoop)
 
 ; helm etags 
-(my-install-package 'helm-etags-plus)
-(require 'helm-etags-plus)
-(global-set-key "\M-." 'helm-etags-plus-select)
- ;;list all visited tags
-(global-set-key "\M-*" 'helm-etags-plus-history)
-;;go back directly
-(global-set-key "\M-," 'helm-etags-plus-history-go-back)
-;;go forward directly
-(global-set-key "\M-/" 'helm-etags-plus-history-go-forward)
+; (my-install-package 'helm-etags-plus)
+; (require 'helm-etags-plus)
+; (global-set-key "\M-." 'helm-etags-plus-select)
+;  ;;list all visited tags
+; (global-set-key "\M-*" 'helm-etags-plus-history)
+; ;;go back directly
+; (global-set-key "\M-," 'helm-etags-plus-history-go-back)
+; ;;go forward directly
+; (global-set-key "\M-/" 'helm-etags-plus-history-go-forward)
 
 (provide 'init-helm)
